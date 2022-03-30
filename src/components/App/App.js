628ec687e11c5f1f15a43ca4f196b0e9366bda5c
Darya Serikova <dvserikova@gmail.com>
2022-03-30 12:55:51 +0700

    if page isn't exist, handle with help Riderect or Route without path

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,7 +16,7 @@ import {
   SecretPage } from '../pages';
 
 // import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Riderect, Redirect} from 'react-router-dom';
 import { StarshipDetails } from '../SWcomponents';
 
 
@@ -61,30 +61,40 @@ export default class App extends Component {
 
               <RandomPlanet updateInterval={5000}/>
 
-              <Route path="/" 
-                    render={() => <h2>Welcome to StarDB</h2>}
-                    exact={true} // можно написать просто true
-                />
-              <Route path="/people/:id?" component={PeoplePage} />
-              <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" exact={true} component={StarshipsPage} />
-              <Route path="/starships/:id"
-                    render={({ match }) => { //{match, location, history}
-                      const { id } = match.params;
-                      return <StarshipDetails itemId={id}/>
-                      }} />
-              <Route 
-                path="/login"
-                render={() => (
-                  <LoginPage 
-                    isLoggedIn={isLoggedIn}
-                    onLogin={this.onLogin}/>
-                )}/>
-              <Route 
-                path="/secret"
-                render={() => (
-                  <SecretPage isLoggedIn={isLoggedIn}/>
-                )}/>
+              <Switch>
+                <Route path="/" 
+                      render={() => <h2>Welcome to StarDB</h2>}
+                      exact={true} // можно написать просто true
+                  />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" exact={true} component={StarshipsPage} />
+                <Route path="/starships/:id"
+                      render={({ match }) => { //{match, location, history}
+                        const { id } = match.params;
+                        return <StarshipDetails itemId={id}/>
+                        }} />
+                <Route 
+                  path="/login"
+                  render={() => (
+                    <LoginPage 
+                      isLoggedIn={isLoggedIn}
+                      onLogin={this.onLogin}/>
+                  )}/>
+                <Route 
+                  path="/secret"
+                  render={() => (
+                    <SecretPage isLoggedIn={isLoggedIn}/>
+                  )}/>
+                {/* Если ни один из Routes не сработал, то Redirect */}
+                {/* <Redirect to="/"/> */}
+                
+                {/* Если мы не передадим в Route path, тогда такой Route
+                 будет срабатывать всегда, а когда он стоит
+                 последний в блоке Switch: срабатывать всегда, когда
+                 ни один другой Route из этого же блока не сработал */}
+                <Route render={() => <h2>Page not found</h2>}/>
+              </Switch>
 
             </div>
           </Router>
